b2f57d9347852f8049f4f50fd23ca59f35eb8cd9
Mike Gerow <gerow@mgerow.com>
2017-12-05 00:54:02 +0000

    Completed day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"sort"
 	"strings"
 )
 
-func main() {
-	s := bufio.NewScanner(os.Stdin)
+func identity(s string) string {
+	return s
+}
+
+// Finds the "canonical" anagram. That is: the anagram sorted by rune value.
+func canonicalAnagram(s string) string {
+	var ss []string
+	for _, r := range s {
+		ss = append(ss, string(r))
+	}
+	sort.Strings(ss)
+	return strings.Join(ss, "")
+}
+
+func numValid(r io.Reader, munger func(string) string) int {
+	s := bufio.NewScanner(r)
 	numValid := 0
 	for s.Scan() {
 		words := strings.Split(s.Text(), " ")
 		used := map[string]bool{}
 		valid := true
 		for _, w := range words {
+			w = munger(w)
 			if _, ok := used[w]; ok {
 				valid = false
 				break
@@ -25,5 +43,14 @@ func main() {
 			numValid++
 		}
 	}
-	fmt.Println("numValid:", numValid)
+
+	return numValid
+}
+
+func main() {
+	var buf bytes.Buffer
+	t := io.TeeReader(os.Stdin, &buf)
+
+	fmt.Println("numValid:", numValid(t, identity))
+	fmt.Println("numValidAnagram:", numValid(&buf, canonicalAnagram))
 }
