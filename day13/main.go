5bb7eebbccbbaefa10ecf2b6fedf0df50ec720f3
Mike Gerow <gerow@mgerow.com>
2017-12-21 04:59:31 +0000

    I feel like this should be a solution, but it loops forever
    I'm going to need to debug this later.

--- day13/main.go
+++ day13/main.go
@@ -13,9 +13,33 @@ type Scanner struct {
 }
 
 type Depth int
+type Scanners map[Depth]*Scanner
+
+func (ss Scanners) Step() {
+	for _, s := range ss {
+		if !s.Reverse {
+			s.Pos++
+		} else {
+			s.Pos--
+		}
+		if s.Pos == 0 {
+			s.Reverse = false
+		} else if s.Pos == (s.Range - 1) {
+			s.Reverse = true
+		}
+	}
+
+}
+
+func (ss Scanners) Reset() {
+	for _, s := range ss {
+		s.Pos = 0
+		s.Reverse = false
+	}
+}
 
 func main() {
-	scanners := map[Depth]*Scanner{}
+	scanners := Scanners{}
 	var maxDepth Depth
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
@@ -36,18 +60,27 @@ func main() {
 		if s, ok := scanners[d]; ok && s.Pos == 0 {
 			severity += int(d) * s.Range
 		}
-		for _, s := range scanners {
-			if !s.Reverse {
-				s.Pos++
-			} else {
-				s.Pos--
-			}
-			if s.Pos == 0 {
-				s.Reverse = false
-			} else if s.Pos == (s.Range - 1) {
-				s.Reverse = true
-			}
-		}
+		scanners.Step()
 	}
 	fmt.Println("Severity:", severity)
+
+	var delay int
+	for ; ; delay++ {
+		scanners.Reset()
+		for n := 0; n < delay; n++ {
+			scanners.Step()
+		}
+		caught := false
+		for d := Depth(0); d <= maxDepth; d++ {
+			if s, ok := scanners[d]; ok && s.Pos == 0 {
+				caught = true
+				break
+			}
+			scanners.Step()
+		}
+		if !caught {
+			break
+		}
+	}
+	fmt.Println("Minimum delay:", delay)
 }
