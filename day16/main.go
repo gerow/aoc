29f184515f6677bd61d64f0b1884917edfc844d8
Mike Gerow <gerow@mgerow.com>
2017-12-24 00:13:28 +0000

    Day 16 part 1

--- day16/main.go
+++ day16/main.go
@@ -1,13 +1,96 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
 func ScanCommas(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	for i := 0; i < len(data); i++ {
-		if data[i] == ',' {
+		if data[i] == ',' || data[i] == '\n' {
 			return i + 1, data[:i], nil
 		}
+	}
+	// If we're at EOF just return the last value.
+	if atEOF {
+		return len(data), data, bufio.ErrFinalToken
 	}
 	return 0, nil, nil
 }
 
+type State struct {
+	Programs []byte
+}
+
+func New(n int) *State {
+	var s State
+	s.Programs = make([]byte, n)
+	for i := range s.Programs {
+		s.Programs[i] = 'a' + byte(i)
+	}
+	return &s
+}
+
+func (s *State) Apply(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(ScanCommas)
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		if t == "" {
+			continue
+		}
+		switch t[0] {
+		case 's':
+			var n int
+			fmt.Sscanf(t, "s%d", &n)
+			s.spin(n)
+		case 'x':
+			var a, b int
+			fmt.Sscanf(t, "x%d/%d", &a, &b)
+			s.exchange(a, b)
+		case 'p':
+			var a, b byte
+			fmt.Sscanf(t, "p%c/%c", &a, &b)
+			s.partner(a, b)
+		default:
+			panic(t)
+		}
+	}
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
+}
+
+func (s *State) spin(n int) {
+	p := make([]byte, len(s.Programs))
+	for i := range p {
+		p[i] = s.Programs[(len(p)-n+i)%len(p)]
+	}
+	s.Programs = p
+}
+
+func (s *State) exchange(a, b int) {
+	s.Programs[a], s.Programs[b] = s.Programs[b], s.Programs[a]
+}
+
+func (s *State) partner(a, b byte) {
+	var ai, bi int
+	for i, v := range s.Programs {
+		if v == a {
+			ai = i
+		}
+		if v == b {
+			bi = i
+		}
+	}
+	s.exchange(ai, bi)
+}
+
 func main() {
+	s := New(16)
+	s.Apply(os.Stdin)
+	fmt.Printf("end state: %q\n", string(s.Programs))
 }
--- day16/main_test.go
+++ day16/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -11,16 +12,39 @@ func TestScanCommas(t *testing.T) {
 		s    string
 		want []string
 	}{
+		{"s1", []string{"s1"}},
 		{"a,b,c,d,e", []string{"a", "b", "c", "d", "e"}},
-		{"ab,cd,ef,gh,ij", []string{"ab", "cd", "ef", "gh", "i"}},
+		{"ab,cd,ef,gh,ij", []string{"ab", "cd", "ef", "gh", "ij"}},
+		{"ab,cd,ef,gh,ij\n", []string{"ab", "cd", "ef", "gh", "ij"}},
 	} {
 		r := strings.NewReader(st.s)
 		s := bufio.NewScanner(r)
 		s.Split(ScanCommas)
-		for i := 0; s.Scan(); i++ {
-			if got := s.Text(); got != st.want[i] {
-				t.Errorf("wrong value for %q at index %d; want %s got %s", st.s, i, st.want[i], got)
+		for i, want := range st.want {
+			s.Scan()
+			if got := s.Text(); got != want {
+				t.Errorf("wrong value for %q at index %d; want %q got %q", st.s, i, want, got)
 			}
+		}
+		if s.Err() != nil {
+			fmt.Errorf("unexpected scan error: %v", s.Err())
+		}
+	}
+}
+
+func TestReference(t *testing.T) {
+	for _, rt := range []struct {
+		moves, want string
+	}{
+		{"s1", "eabcd"},
+		{"s2", "deabc"},
+		{"s1,x3/4,pe/b", "baedc"},
+	} {
+		s := New(len(rt.want))
+		r := strings.NewReader(rt.moves)
+		s.Apply(r)
+		if got := string(s.Programs); got != rt.want {
+			t.Errorf("wrong result for moves %q; want %q got %q", rt.moves, rt.want, got)
 		}
 	}
 }
