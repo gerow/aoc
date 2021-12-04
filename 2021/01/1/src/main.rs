99ba0ec323b4e54dba2ee232ba6b662989305da2
Mike Gerow <gerow@mgerow.com>
2021-12-04 02:52:36 +0000

    2021 day 1 part 1

---
+++ 2021/01/1/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::BufRead;
+use std::error::Error;
+use std::io;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut increased = 0;
+    let mut first = true;
+    let mut prev = 0;
+
+    for l in io::stdin().lock().lines() {
+        let l = l?;
+        let n = l.parse::<i32>()?;
+        if first {
+            prev = n;
+            first = false;
+            continue;
+        }
+
+        if n > prev {
+            increased += 1;
+        }
+        prev = n;
+    }
+
+    println!("{}", increased);
+
+    Ok(())
+}
